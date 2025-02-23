6ce7b6bcaea3f0bd53ccf455f9fb9eef33147623
jbdmc <joaocorreia.formando@gmail.com>
2025-02-23 00:13:37 +0000

    added c00-repo

---
+++ c00-repo/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbdmc <jbdmc@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 13:46:29 by jbdmc             #+#    #+#             */
+/*   Updated: 2025/02/22 15:28:31 by jbdmc            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char c = 122;
+	while(c > 96)
+	{
+		write(1, &c, 1);
+		c--;
+	}
+}
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
